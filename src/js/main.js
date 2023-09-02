e1ced2345a7c0e8a4b4b7c3922fe14324038b863
lucas matias <lucasmlima1516@gmail.com>
2023-09-02 19:18:28 -0300

    feat(init): initial commit project

---
+++ src/js/main.js
@@ -0,0 +1,40 @@
+/*############### SHOW MENU MOBILE ###############*/
+const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close")
+
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu")
+    })
+}
+
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu")
+    })
+}
+
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu");
+
+    navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach(n => n.addEventListener("click", linkAction));
+
+/*############### HOME SWIPER ###############*/
+
+
+/*############### CHANGE BG HEADER ###############*/
+const scrollHeader = () => {
+    const header = document.getElementById("header");
+
+    this.scrollY >= 50 ? header.classList.add("bg-header")
+                       : header.classList.remove("bg-header")
+}
+window.addEventListener("scroll", scrollHeader);
+
+/*############### ACTIVE LINK ###############*/
